3b39928bfe20ec840cf8796f6e3e3db13b557a3f
Yash Shukla <56587507+Yash1256@users.noreply.github.com>
2020-03-27 15:30:10 +0530

    Fix an issue

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'posts.settings')
     try:
-        from django.core.managemsssent import execute_from_command_line
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
